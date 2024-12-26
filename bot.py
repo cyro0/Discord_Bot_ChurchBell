9836b9b074f59fa876d3d9eaf969a739901d6b68
cyro0 <chefbigpasta@gmx.net>
2024-12-26 17:00:25 +0100

    added ffmpeg and playing sound in vc

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import datetime
 import discord
+import asyncio
 from discord.ext import tasks, commands
 from constants import TOKEN
 
 # fill list with every datetime hour for join_vc loop
 times = []
 for i in range(24):
-    for y in range(60):
-        times.append(datetime.time(hour=i, minute=y))
+    times.append(datetime.time(hour=i))
 
 def run_bot():
     BOT_TOKEN = TOKEN
@@ -39,5 +39,25 @@ def run_bot():
     @tasks.loop(time=times)
     async def join_vc():
         print("Die Stunde hat geschlagen")
+        all_guilds = client.guilds
+
+        v_channels = []
+        for i in all_guilds:
+            for y in i.categories:
+                if len(y.voice_channels) > 0:
+                    v_channels.extend(y.voice_channels)
+        
+        channels_with_members = []
+        for channel in v_channels:
+            if len(channel.members) > 0:
+                channels_with_members.append(channel)
+
+        voice_client = await channels_with_members[0].connect()
+        bell_sound_effect = discord.FFmpegOpusAudio(source="church_bell.ogg", executable="ffmpeg-master-latest-win64-gpl\\bin\\ffmpeg.exe")
+
+        voice_client.play(bell_sound_effect)
+        await asyncio.sleep(5)
+
+        await voice_client.disconnect()
 
     client.run(BOT_TOKEN)
