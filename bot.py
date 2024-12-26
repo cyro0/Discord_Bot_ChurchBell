e76f5b1baa177c94cb5054e862232d992281da54
cyro0 <chefbigpasta@gmx.net>
2024-12-26 13:42:14 +0100

    added on ping message and task looping
    @ChurchBell can now be pinged to send the current time in the text channel it was pinged
    
    task looping as a test to see if the churchbell prints every full minute of the day

--- bot.py
+++ bot.py
@@ -1,4 +1,43 @@
+import datetime
+import discord
+from discord.ext import tasks, commands
 from constants import TOKEN
+
+# fill list with every datetime hour for join_vc loop
+times = []
+for i in range(24):
+    for y in range(60):
+        times.append(datetime.time(hour=i, minute=y))
 
 def run_bot():
     BOT_TOKEN = TOKEN
+
+    intents = discord.Intents.all()
+
+    client = discord.Client(intents=intents)
+    
+    @client.event
+    async def on_ready():
+        print(f"{client.user} is running!")
+        join_vc.start()
+    
+    @client.event
+    async def on_message(message):
+        if len(message.mentions) > 0 and message.mentions[0].id == 1316768540572782643:
+
+            author = str(message.author)
+            channel = str(message.channel)
+
+            print(f"{author} mentioned {client.user} in {channel}")
+            try:
+                response = str(datetime.datetime.now())
+                await message.channel.send(response)
+            
+            except Exception as e:
+                print(e)
+
+    @tasks.loop(time=times)
+    async def join_vc():
+        print("Die Stunde hat geschlagen")
+
+    client.run(BOT_TOKEN)
